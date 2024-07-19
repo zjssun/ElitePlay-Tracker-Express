0ebe468febe0a73ad549e7d7c62534da0de46bc8
SamRol <1628526495@qq.com>
2024-07-19 23:52:10 +0800

    Add an interface to query match information by time

--- routers/GetMatch.js
+++ routers/GetMatch.js
@@ -13,28 +13,17 @@ router.get('/all',async (req,res)=>{
       // if cache didn't exists
       if(!alldata || alldata.length === 0){
          const query = `  SELECT * FROM (
-                  SELECT * FROM donk
-                  UNION ALL
-                  SELECT * FROM elige
-                  UNION ALL
-                  SELECT * FROM jame
-                  UNION ALL
-                  SELECT * FROM m0nesy
-                  UNION ALL
-                  SELECT * FROM ropz
-                  UNION ALL
-                  SELECT * FROM im
-                  UNION ALL
-                  SELECT * FROM jks
-                  UNION ALL
-                  SELECT * FROM niko
-                  UNION ALL
-                  SELECT * FROM jl
-                  UNION ALL
-                  SELECT * FROM s1mple
-                  UNION ALL
-                  SELECT * FROM w0nderful
-                  UNION ALL
+                  SELECT * FROM donk UNION ALL
+                  SELECT * FROM elige UNION ALL
+                  SELECT * FROM jame UNION ALL
+                  SELECT * FROM m0nesy UNION ALL
+                  SELECT * FROM ropz UNION ALL
+                  SELECT * FROM im UNION ALL
+                  SELECT * FROM jks UNION ALL
+                  SELECT * FROM niko UNION ALL
+                  SELECT * FROM jl UNION ALL
+                  SELECT * FROM s1mple UNION ALL
+                  SELECT * FROM w0nderful UNION ALL
                   SELECT * FROM zywoo
                   ) AS combined_tables
                   ORDER BY time DESC;
@@ -50,6 +39,10 @@ router.get('/all',async (req,res)=>{
       });
    }catch(err){
       console.error(err);
+      res.send({
+         message: "error",
+         error: err
+      });
    }
 
 });
@@ -80,4 +73,44 @@ router.get('/:playerName',async (req,res)=>{
    }
 })
 
+// get data by match Time
+router.post('/matchTime',async (req,res)=>{
+   const {time} = req.body;
+   const matchTimeKey = `SELECT${time}`;
+   try{
+      let playerData = await get(matchTimeKey);
+      if(!playerData || playerData.length === 0){
+         const query = `
+            SELECT * FROM(
+               SELECT * FROM donk WHERE time LIKE '%${time}%' UNION ALL
+               SELECT * FROM im WHERE time LIKE '%${time}%' UNION ALL
+               SELECT * FROM elige WHERE time LIKE '%${time}%' UNION ALL
+               SELECT * FROM jame WHERE time LIKE '%${time}%' UNION ALL
+               SELECT * FROM jks WHERE time LIKE '%${time}%' UNION ALL
+               SELECT * FROM jl WHERE time LIKE '%${time}%' UNION ALL
+               SELECT * FROM m0nesy WHERE time LIKE '%${time}%' UNION ALL
+               SELECT * FROM niko WHERE time LIKE '%${time}%' UNION ALL
+               SELECT * FROM ropz WHERE time LIKE '%${time}%' UNION ALL
+               SELECT * FROM s1mple WHERE time LIKE '%${time}%' UNION ALL
+               SELECT * FROM w0nderful WHERE time LIKE '%${time}%' UNION ALL
+               SELECT * FROM zywoo WHERE time LIKE '%${time}%'
+            )AS combined_tables ORDER BY time DESC
+         `
+         // execute query and get playerdata
+         playerData = await Connect.query(query);
+         // set data to cache
+         await set(matchTimeKey,playerData);
+      }
+      res.send({
+         message: "success",
+         data: playerData[0]
+      });
+   }catch(err){
+      console.error("Get MatchTime Error:",err);
+      res.send({
+         message: "error",
+         error: err
+      })
+   }
+})
 module.exports = router;
