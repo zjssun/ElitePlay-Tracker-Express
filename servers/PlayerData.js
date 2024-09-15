8a9f7ddb0c0e8a9fc93419e41c926e2b7760e727
SamRol <1628526495@qq.com>
2024-09-15 23:03:02 +0800

    fix player's adr data

--- servers/PlayerData.js
+++ servers/PlayerData.js
@@ -10,17 +10,26 @@ const getData = async (data)=>{
       if(!data) return [];
       const promises = data.map(async (v)=>{
          const MatchRoom = `https://www.faceit.com/api/match/v2/match/${v.matchId}`;
+         const MatchDetal = `https://www.faceit.com/api/stats/v1/stats/matches/${v.matchId}`;
          let effectiveRanking = "0";
+         let playerAdr = "";
          if(v.bestOf === "2"){
             const Room = await axios.get(MatchRoom);
             effectiveRanking = Room.data.payload.entityCustom.effectiveRanking;
+            const Detal = await axios.get(MatchDetal);
+            const Details = JSON.stringify(Detal.data);
+            const teamKeyIndex = Details.indexOf('"teams":');
+            const teamContent = JSON.parse(`{${Details.slice(teamKeyIndex,-1)}`);
+            const PlayerInTeam = teamContent.teams.find(team=>team.teamId === v.teamId);
+            const targetPlayer = PlayerInTeam.players.find(player=>player.playerId === v.playerId);
+            playerAdr = targetPlayer.c10 || "unstats";
          }else{
              effectiveRanking = "0";
          }
          return{
             time:GetTime(v.created_at,'YYYY/MM/DD,hh:mm:ss'),
             nickname:v.nickname,
-            playerId:v._id.playerId,
+            playerId:v.playerId,
             team:v.i5,
             matchMap:v.i1,
             matchScore:v.i18,
@@ -33,7 +42,7 @@ const getData = async (data)=>{
             totalDeaths:v.i8,
             totalAssistsL:v.i7,
             rating:formatRating(v.c2),
-            adr:v.c10 || "unstats",
+            adr:playerAdr,
             tripleKill:v.i14 || "0",
             quadroKill:v.i15 || "0",
             pentaKill:v.i16 || "0",
@@ -51,7 +60,7 @@ const getPlayerDate = async () => {
    try {
       const promises = PlayerList.map(async (v) => {
          const playerId = v.split(' ')[1];
-         const response = await axios.get(`https://www.faceit.com/api/stats/v1/stats/time/users/${playerId}/games/cs2?page=0&size=10&game_mode=5v5`);
+         const response = await axios.get(`https://www.faceit.com/api/stats/v1/stats/time/users/${playerId}/games/cs2?page=0&size=3&game_mode=5v5`);
          const matchHistory = response.data;
          let playerData = await getData(matchHistory);
          FinalData = [...FinalData, ...playerData];
